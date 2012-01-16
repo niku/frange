70e0cdd770d30ececa5c1eecc46321a9c9a0412e
niku <niku@niku.name>
2012-01-17 08:23:55 +0900

    pack files

--- lib/frange.rb
+++ lib/frange.rb
@@ -1,16 +1,74 @@
 require "frange/version"
 
 module Frange
-  autoload :Pipe,    "frange/pipe"
-  autoload :Builder, "frange/builder"
+  class Draft
+    attr_accessor :selector, :source
+    attr_reader :filters
 
-  def self.piping &block
-    block_given? ? drafting(&block).new : drafting.new
+    def initialize
+      @selector = ->(input){ true }
+      @source = Enumerator.new {}
+      @filters = []
+    end
+
+    def add_filter filter
+      @filters << filter
+    end
   end
 
-  def self.drafting &block
+  class Pipe
+    def initialize draft
+      @draft = draft
+    end
+
+    def new source = nil
+      source   = source || @draft.source.clone
+      selector = @draft.selector
+      filters  = @draft.filters
+      Valve.new { |y|
+        loop do
+          source.next until selector.call(source.peek)
+          y << filters.reduce(source.next){ |s,f| f.call(s) }
+        end
+      }
+    end
+  end
+
+  class Valve < Enumerator; end
+
+  class Builder
+    def initialize
+      @draft = Draft.new
+    end
+
+    def filter &block
+      @draft.add_filter block
+    end
+
+    def source val
+      @draft.source = val
+    end
+
+    def selector &block
+      @draft.selector = block
+    end
+
+    def to_pipe
+      Pipe.new(@draft)
+    end
+  end
+
+  def self.pipe &block
     builder = Builder.new
     builder.instance_eval &block if block_given?
     builder.to_pipe
+  end
+
+  def self.valve &block
+    if block_given?
+      self.pipe(&block).new
+    else
+      self.pipe.new
+    end
   end
 end
--- spec/frange_spec.rb
+++ spec/frange_spec.rb
@@ -1,10 +1,53 @@
 require_relative "spec_helper"
 
-describe Frange do
+module Frange
+  describe Pipe do
+    describe "#next" do
+      context "when default" do
+        it{ expect{ subject.next }.to raise_exception } # TODO How to behave?
+      end
+      context "given source" do
+        subject do
+          draft = Draft.new
+          draft.source = %w(a b).to_enum
+          pipe = Pipe.new(draft)
+          pipe.new
+        end
+        context "when called twice" do
+          it{
+            ary = []
+            2.times{ ary << subject.next }
+            ary.should eq ["a", "b"]
+          }
+        end
+        context "when called 3 times" do
+          it{ expect{ 3.times{ subject.next } }.to raise_exception(StopIteration) }
+        end
+      end
+      context "given source and filter" do
+        subject do
+          draft = Draft.new
+          draft.source = %w(a b).to_enum
+          draft.add_filter ->(input){ input + "1" }
+          draft.add_filter ->(input){ input + "2" }
+          pipe = Pipe.new(draft)
+          pipe.new
+        end
+        context "when called twice" do
+          it{
+            ary = []
+            2.times{ ary << subject.next }
+            ary.should eq ["a12", "b12"]
+          }
+        end
+      end
+    end
+  end
+
   describe "Frange.piping" do
     context "given with source" do
       subject {
-        Frange.piping { |pipe|
+        Frange.valve { |pipe|
           pipe.source ["a", 1, "b", 2, "c"].to_enum
           pipe.selector { |input| input.kind_of?(String) }
           pipe.filter { |input| input + "1" }
@@ -12,7 +55,7 @@ describe Frange do
           pipe.filter { |input| input + "3" }
         }
       }
-      it{ should be_kind_of Frange::Pipe }
+      it{ should be_kind_of Frange::Valve }
       it{ subject.should have(3).filters }
       it{ subject.next.should eq "a123" }
     end
