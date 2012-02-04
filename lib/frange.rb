a597d96365724e982c9558e6f758dff213ab39dc
niku <niku@niku.name>
2012-02-04 20:47:07 +0900

    modularize for suppress namespace collision

--- lib/frange.rb
+++ lib/frange.rb
@@ -2,52 +2,54 @@ require "frange/version"
 
 module Frange
   def self.draft &block
-    draft = Draft.new
+    draft = Core::Draft.new
     draft.config = block
     draft
   end
 
-  class Draft
-    attr_accessor :config
+  module Core
+    class Draft
+      attr_accessor :config
 
-    def build params = {}
-      pipe = Pipe.new
-      pipe.params = params
-      pipe.instance_eval &config
-      pipe
-    end
-  end
-
-  class Pipe < Enumerator
-    attr_accessor :params
-
-    def initialize
-      @source = Enumerator.new([])
-      @selector = ->(input){ true }
-      @filters = []
-      super() { |y|
-        loop do
-          @source.next until @selector.call(@source.peek)
-          y << @filters.reduce(@source.next){ |s,f| f.call(s) }
-        end
-      }
+      def build params = {}
+        pipe = Pipe.new
+        pipe.params = params
+        pipe.instance_eval &config
+        pipe
+      end
     end
 
-    protected
-    def filter &block
-      @filters << block
-    end
+    class Pipe < Enumerator
+      attr_accessor :params
 
-    def source obj=[], method = :each, *args, &block
-      @source = case
-                when block_given?; Enumerator.new &block
-                when *args.empty?; Enumerator.new obj, method
-                else Enumerator.new obj, method, *args
-                end
-    end
+      def initialize
+        @source = Enumerator.new([])
+        @selector = ->(input){ true }
+        @filters = []
+        super() { |y|
+          loop do
+            @source.next until @selector.call(@source.peek)
+            y << @filters.reduce(@source.next){ |s,f| f.call(s) }
+          end
+        }
+      end
 
-    def selector &block
-      @selector = block
+      protected
+      def filter &block
+        @filters << block
+      end
+
+      def source obj=[], method = :each, *args, &block
+        @source = case
+                  when block_given?; Enumerator.new &block
+                  when *args.empty?; Enumerator.new obj, method
+                  else Enumerator.new obj, method, *args
+                  end
+      end
+
+      def selector &block
+        @selector = block
+      end
     end
   end
 end
